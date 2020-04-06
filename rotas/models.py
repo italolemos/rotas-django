621106640f094b47ba93f63f63c04a7ef3d04a49
Ítalo Lemos <italo_vinicius13@yahoo.com.br>
2020-04-05 23:19:58 -0300

    Add Pipenv

--- rotas/models.py
+++ rotas/models.py
@@ -5,6 +5,8 @@ class Rotas(models.Model):
     nome = models.CharField('Nome', max_length=50)
     descricao = models.CharField('Descrição', max_length=500)
     tamanho = models.DecimalField('Tamanho da Rota em Km', max_digits=4, decimal_places=2)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
